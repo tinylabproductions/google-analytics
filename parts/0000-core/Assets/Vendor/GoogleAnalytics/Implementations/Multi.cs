e7189fbedc66467b9ab25482a813c14871950e62
Arturas Slajus <arturas@tinylabproductions.com>
2016-07-01 19:31:22 +0300

    MultiGAClient - send events to multiple client implementations.

---
+++ parts/0000-core/Assets/Vendor/GoogleAnalytics/Implementations/Multi.cs
@@ -0,0 +1,23 @@
+﻿namespace com.tinylabproductions.GoogleAnalytics.Implementations {
+  public class MultiGAClient : IGAClient {
+    readonly IGAClient[] clients;
+
+    public MultiGAClient(IGAClient[] clients) { this.clients = clients; }
+
+    public void Event(GAEvent data) {
+      foreach (var c in clients) c.Event(data);
+    }
+
+    public void AppView(GAAppView data) {
+      foreach (var c in clients) c.AppView(data);
+    }
+
+    public void Item(GAItem data) {
+      foreach (var c in clients) c.Item(data);
+    }
+
+    public void Timing(GATiming data) {
+      foreach (var c in clients) c.Timing(data);
+    }
+  }
+}
