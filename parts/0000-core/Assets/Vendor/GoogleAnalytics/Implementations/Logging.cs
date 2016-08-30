9770681f2caa491405ead5a7f6827bbafb2c00db
Arturas Slajus <arturas@tinylabproductions.com>
2016-08-30 18:43:08 +0300

    Provide logging client implementation.

---
+++ parts/0000-core/Assets/Vendor/GoogleAnalytics/Implementations/Logging.cs
@@ -0,0 +1,25 @@
+﻿using com.tinylabproductions.TLPLib.Logger;
+
+namespace com.tinylabproductions.GoogleAnalytics.Implementations {
+  public class GAClientLoggingImpl : IGAClient {
+    public const Log.Level DEFAULT_LEVEL = Log.Level.DEBUG;
+
+    readonly ILog logger;
+    readonly Log.Level level;
+
+    public GAClientLoggingImpl(ILog logger, Log.Level level = DEFAULT_LEVEL) {
+      this.logger = logger;
+      this.level = level;
+    }
+
+    public GAClientLoggingImpl(Log.Level level = DEFAULT_LEVEL) 
+      : this(Log.defaultLogger, level) {}
+
+    public void Event(GAEvent data) { if (logger.willLog(level)) log(data.ToString()); }
+    public void AppView(GAAppView data) { if (logger.willLog(level)) log(data.ToString()); }
+    public void Item(GAItem data) { if (logger.willLog(level)) log(data.ToString()); }
+    public void Timing(GATiming data) { if (logger.willLog(level)) log(data.ToString()); }
+
+    void log(string s) => logger.debug($"[Google Analytics] {s}");
+  }
+}
